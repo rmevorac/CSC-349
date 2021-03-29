9eb12d1ebb2fe8b5783c57c5fa0d3e4862e8948c
Tylertyca <tylertycahart@gmail.com>
2021-03-29 10:12:25 -0700

    Update Lab0.java

--- Lab0/Lab0.java
+++ Lab0/Lab0.java
@@ -1,8 +1,74 @@
 //Author: Roey Mevorach & Tyler Hart
 
 import java.util.*;
+import java.lang.*;
 
 class Algorithms {
+	public static void printLinearTime()
+	{
+		long startTime;
+		long endTime;
+		for(long i = 1000; i < 100000001; i*=2)
+		{
+			startTime = System.currentTimeMillis();
+			linearAlgorithm(i);
+		        endTime = System.currentTimeMillis();
+		        endTime = endTime - startTime;
+			System.out.println("T(" + i + ") = " + endTime);
+		}
+	}
+	public static void printQuadraticTime()
+	{
+		long startTime;
+		long endTime;
+		for(long i = 1000; i < 100000001; i*=2)
+		{
+			startTime = System.currentTimeMillis();
+			quadraticAlgorithm(i);
+			endTime = System.currentTimeMillis();
+			endTime = endTime - startTime;
+			System.out.println("T(" + i + ") = " + endTime);
+		}
+	}
+	public static void printCubicTime()
+	{
+		long startTime;
+		long endTime;
+		for(long i = 1000; i < 100000001; i *=2)
+		{
+			startTime = System.currentTimeMillis();
+			cubicAlgorithm(i);
+			endTime = System.currentTimeMillis();
+			endTime = endTime - startTime;
+			System.out.println("T(" + i + ") = " + endTime);
+		}
+	}
+	public static void printLogTime()
+	{
+		long startTime;
+		long endTime;
+		for(long i = 1000; i < 100000001; i*=2)
+		{
+			startTime = System.currentTimeMillis();
+			logarithmicAlgorithm(i);
+			endTime = System.currentTimeMillis();
+			endTime = endTime - startTime;
+			System.out.println("T(" + i + ") = " + endTime);
+		}
+	}
+	public static void printLogNTime()
+	{
+		long startTime;
+		long endTime;
+		for(long i = 1000; i < 100000001; i*=2)
+		{
+			startTime = System.currentTimeMillis();
+			NlogNAlgorithm(i);
+			endTime = System.currentTimeMillis();
+			endTime = endTime - startTime;
+			System.out.println("T)" + i + ") = " + endTime);
+		}
+	}
    public static  void linearAlgorithm(long N) {
       int x;
       for (long i = 1; i < N; i++) {
@@ -45,4 +111,17 @@ class Algorithms {
          }
       }
    }
+	public static void main(String[] args)
+	{
+		System.out.println("Linear algorithm's running times:");
+		printLinearTime();
+		System.out.println("Quadratic algorithm's running times:");
+		printQuadraticTime();
+		System.out.println("Cubic algorithm's running times:");
+		printCubicTime();
+		System.out.println("Logarithmic algorithm's running times:");
+		printLogTime();
+		System.out.println("Nlog(N) algorithm's running times:");
+		printLogNTime();
+	}
 }
