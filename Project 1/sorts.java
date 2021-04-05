c1bae46ae8ba2be4fecc7ec108e4060814e0ee31
rmevorac <56085595+rmevorac@users.noreply.github.com>
2021-04-05 14:13:59 -0700

    Add files via upload

---
+++ Project 1/sorts.java
@@ -0,0 +1,24 @@
+//Roey Mevorach and Tyler Hart
+
+import java.util.*;
+
+class Sorts {
+   public static void selectionSort (int[] arr, int N) {
+      //Sorts the list of N elements contained in arr[0..N-1] using the selection sort algorithm.
+   }
+
+   public static void bubbleSort (int[] arr, int N) {
+      //Sorts the list of N elements contained in arr[0..N-1] using the improved bubble sort algorithm
+      //(see the handout).
+   }
+
+   public static void mergeSort (int[] arr, int N) {
+      //Sorts the list of N elements contained in arr[0..N-1] using the merge sort algorithm.
+   }
+
+   public static void quickSort (int[] arr, int N) {
+      //Sorts the list of N elements contained in arr[0..N-1] using the quick sort algorithm with
+      //median-of-three pivot and rearrangement of the three elements (see the handout).
+   }
+}
+
