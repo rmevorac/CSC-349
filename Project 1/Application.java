53c1d3004c5631549f0ebfa32c9388c88535a2fc
Tylertyca <tylertycahart@gmail.com>
2021-04-12 10:26:06 -0700

    Update Application.java

--- Project 1/Application.java
+++ Project 1/Application.java
@@ -31,18 +31,16 @@ class Application {
       System.out.print("\n");
    }
 
-   public void testBubbleSort(int N) {
-      int[] arr = createArray(N);
+   public void testBubbleSort(int N, int[] arr) {
       //printArray(arr);
       long x;
       long t = System.currentTimeMillis();
       Sorts.bubbleSort(arr, arr.length);
       x = System.currentTimeMillis();
-      printArray(arr);
+      System.out.print("T_bs=" + (x-t) + ", ");
    }
 
-   public void testSelectionSort(int N) {
-      int[] arr = createArray(N);
+   public void testSelectionSort(int N, int[] arr) {
       //printArray(arr);
       long x;
       long t = System.currentTimeMillis();
@@ -52,27 +50,43 @@ class Application {
       //printArray(arr);
    }
 
-   public void testMergeSort(int N) {
-      int[] arr = createArray(N);
-      printArray(arr);
-
+   public void testMergeSort(int N, int[] arr) {
+      long x;
+      long t = System.currentTimeMillis();
       Sorts.mergeSort(arr, arr.length);
-      printArray(arr);
+      x = System.currentTimeMillis();
+      System.out.print("T_ms="+(x-t) + ", ");
    }
 
-   public void testQuickSort(int N) {
-      int[] arr = createArray(N);
-      printArray(arr);
-
+   public void testQuickSort(int N, int[] arr) {
+      long x;
+      long t = System.currentTimeMillis();
       Sorts.quickSort(arr, arr.length);
-      printArray(arr);
+      x = System.currentTimeMillis();
+      System.out.println("T_qs=" + (x-t));
    }
 
 
    public static void main(String[] args) {
       Application app = new Application();
-      System.out.print("N=5000: ");
-      app.testSelectionSort(5000);
-      app.testBubbleSort(5000);
+      int[] arr;
+      int[] temp;
+      for(int i = 5000; i <= 160000; i*=2)
+      {
+         for(int q = 1; q <= 5; q++)
+         {
+            arr = app.createArray(i);
+            temp = arr.clone();
+            System.out.print("N=" + i + ": ");
+            app.testSelectionSort(i,arr);
+            arr = temp.clone();
+            app.testBubbleSort(i,arr);
+            arr = temp.clone();
+            app.testMergeSort(i,arr);
+            arr = temp.clone();
+            app.testQuickSort(i,arr);
+         }
+         System.out.println();
+      }
    }
 }
