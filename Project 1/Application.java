64fac5c93081ee546bfb70578bb49b9380c7545f
Tyler <tylertycahart@gmail.com>
2021-04-12 14:37:24 -0700

    printing edit

--- Project 1/Application.java
+++ Project 1/Application.java
@@ -7,15 +7,15 @@ class Application {
    public Application() {
    }
 
-   private static int randomFill() {
-      return new Random().nextInt(50);
+   private static int randomFill(int N) {
+      return new Random().nextInt(N-1);
   }
 
-   public int[] createArray() {
-      int[] arr = new int[10];
+   public int[] createArray(int N) {
+      int[] arr = new int[N];
 
       for (int i = 0; i < arr.length; i++) {
-         arr[i] = randomFill();
+         arr[i] = randomFill(N);
       }
 
       return arr;
@@ -31,41 +31,63 @@ class Application {
       System.out.print("\n");
    }
 
-   public void testBubbleSort() {
-      int[] arr = createArray();
-      printArray(arr);
-
+   public void testBubbleSort(int N, int[] arr) {
+      //printArray(arr);
+      long x;
+      long t = System.currentTimeMillis();
       Sorts.bubbleSort(arr, arr.length);
-      printArray(arr);
+      x = System.currentTimeMillis();
+      System.out.print("T_bs=" + (x-t) + ", ");
    }
 
-   public void testSelectionSort() {
-      int[] arr = createArray();
-      printArray(arr);
-
+   public void testSelectionSort(int N, int[] arr) {
+      //printArray(arr);
+      long x;
+      long t = System.currentTimeMillis();
       Sorts.selectionSort(arr, arr.length);
-      printArray(arr);
+      x = System.currentTimeMillis();
+      System.out.print("T_ss=" + (x-t) + ", ");
+      //printArray(arr);
    }
 
-   public void testMergeSort() {
-      int[] arr = createArray();
-      printArray(arr);
-
+   public void testMergeSort(int N, int[] arr) {
+      long x;
+      long t = System.currentTimeMillis();
       Sorts.mergeSort(arr, arr.length);
-      printArray(arr);
+      x = System.currentTimeMillis();
+      System.out.print("T_ms="+(x-t) + ", ");
    }
 
-   public void testQuickSort() {
-      int[] arr = createArray();
-      printArray(arr);
-
+   public void testQuickSort(int N, int[] arr) {
+      long x;
+      long t = System.currentTimeMillis();
       Sorts.quickSort(arr, arr.length);
-      printArray(arr);
+      x = System.currentTimeMillis();
+      System.out.println("T_qs=" + (x-t));
    }
 
 
    public static void main(String[] args) {
       Application app = new Application();
-      app.testSelectionSort();
+      int[] arr;
+      int[] temp;
+      for(int i = 5000; i <= 160000; i*=2)
+      {
+         for(int q = 1; q <= 5; q++)
+         {
+            arr = app.createArray(i);
+            temp = arr.clone();
+            System.out.print("N=" + i + ": ");
+            app.testSelectionSort(i,arr);
+            arr = temp.clone();
+            app.testBubbleSort(i,arr);
+            arr = temp.clone();
+            app.testMergeSort(i,arr);
+            arr = temp.clone();
+            app.testQuickSort(i,arr);
+         }
+         if(i != 160000)
+            System.out.println();
+      }
    }
 }
--- Project 1/sorts.java
+++ Project 1/sorts.java
@@ -3,6 +3,24 @@
 import java.util.*;
 
 class Sorts {
+
+   public static void printArray(int[] arr) {
+      for (int i = 0; i < arr.length; i++) {
+         System.out.print(arr[i]);
+         if (i < arr.length - 1) {
+            System.out.print(", ");
+         }
+      }
+      System.out.print("\n");
+   }
+
+   public static int[] swapElements(int[] arr, int idx1, int idx2) {
+      int temp = arr[idx1];
+      arr[idx1] = arr[idx2];
+      arr[idx2] = temp;
+      return arr;
+   }
+
    public static void selectionSort (int[] arr, int N) {
       //Sorts the list of N elements contained in arr[0..N-1] using the selection sort algorithm.
       int min;
@@ -27,6 +45,18 @@ class Sorts {
    public static void bubbleSort (int[] arr, int N) {
       //Sorts the list of N elements contained in arr[0..N-1] using the improved bubble sort algorithm
       //(see the handout).
+      boolean flag = true;
+      int last = N - 1;
+      while (flag) {
+         flag = false;
+         for (int i = 0; i < last; i++) {
+            if (arr[i] > arr[i + 1]) {
+               arr = swapElements(arr, i, i + 1);
+               flag = true;
+            }
+         }
+         last -= 1;
+      }
    }
 
    public static void mergeSort (int[] arr, int N) {
@@ -87,6 +117,74 @@ class Sorts {
    public static void quickSort (int[] arr, int N) {
       //Sorts the list of N elements contained in arr[0..N-1] using the quick sort algorithm with
       //median-of-three pivot and rearrangement of the three elements (see the handout).
+      quickSort(arr, 0, N - 1);
+   }
+
+   private static void quickSort (int[] list, int first, int last) { //sorts list[first..last] segment
+      if (first < last) { //checking if there is more than one element in list[first..last] segment
+         setPivotToEnd(list, first, last); //supporting method
+         int pivotIndex = splitList (list, first, last); //supporting method quickSort(list, first, pivotIndex-1);
+         quickSort(list, first, pivotIndex - 1);
+         quickSort(list, pivotIndex + 1, last);
+      }
+   }
+
+   private static void setPivotToEnd (int[] arr, int left, int right) {
+      if (arr[left] > arr[(left + right) / 2]) {
+         arr = swapElements(arr, left, (left + right) / 2);
+      }
+
+      if (arr[left] > arr[right]) {
+         arr = swapElements(arr, left, right);
+      }
+
+      if (arr[right] > arr[(left + right) / 2]) {
+         arr = swapElements(arr, (left + right) / 2, right);
+      }
+
+      // System.out.print("After setPivotToEnd: ");
+      // printArray(arr);
+   }
+
+   private static int splitList (int[] arr, int left, int right) {
+      //Rearranges the list by placing the pivot so that it is preceded by smaller
+      //values and followed by greater values. Returns pivot’s index.
+      //Precondition: arr[right] contains the pivot
+      //Postcondition: the pivot is properly placed and its index is returned.
+      // Elements in the list are arranged so that arr[i]<pivot for all arr[i]
+      // located to the left of pivot, and arr[i]>pivot for all arr[i] located to
+      // the right of the pivot.
+      int indexL = left;
+      int indexR = right - 1;
+      int pivot = right;
+
+      while (indexL < indexR) {
+         // System.out.println("before left, right, pivot: " + indexL + ", " + indexR + ", " + pivot);
+         // System.out.println("arr left, arr right, arr pivot: " + arr[indexL] + ", " + arr[indexR] + ", " + arr[pivot]);
+         while (arr[indexL] < arr[pivot]) {
+            indexL++;
+         }
+
+         while (arr[indexR] > arr[pivot] && indexL < indexR) {
+            indexR--;
+         }
+
+         if (indexL < indexR) {
+            arr = swapElements(arr, indexL, indexR);
+            indexL++;
+            indexR--;
+         }
+         // System.out.println("after left, right, pivot: " + indexL + ", " + indexR + ", " + pivot);
+         // printArray(arr);
+      }
+
+      if (indexL == indexR && arr[indexL] <= arr[pivot]) {
+         indexL++;
+      }
+
+      arr = swapElements(arr, indexL, pivot);
+      // System.out.print("After splitList: ");
+      // printArray(arr);
+      return indexL;
    }
 }
-
