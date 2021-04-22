226561e388a48562bb0752f48b97e2b68a4c6ecc
rmevorac <rmevorac@calpoly.edu>
2021-04-21 18:28:13 -0700

    Project2 Part 2

---
+++ Project2_part2/MatrixProduct.java
@@ -0,0 +1,16 @@
+// Roey Mevorach and Tyler Hart
+// rmevorac and tyhart
+// 4/23/2021
+// Project1
+
+import java.util.*;
+
+class MatrixProduct {
+   public static int[][] matrixProduct_DAC(int[][] A, int[][] B) {
+      //Compute and return the product of A, B matrices using “simple” DAC algorithm presented in class.
+      
+   }
+   public static int[][] matrixProduct_Strassen(int[][] A, int[][] B) {
+      //Compute and return the product of A, B matrixes using Strassen’s algorithm presented in class.
+   }
+}
