21b1be984bbf19ce175b94b3fafb4f81f89973f7
rmevorac <rmevorac@calpoly.edu>
2021-05-16 18:16:51 -0700

    updated main method and contains greedy approach

--- Project4/ChangeMaker.java
+++ Project4/ChangeMaker.java
@@ -1,7 +1,7 @@
 // Roey Mevorach and Tyler Hart
 // rmevorac and tyhart
 // 5/17/2021
-// Project4
+// Project 4
 
 import java.util.*;
 import java.io.File;
@@ -16,78 +16,126 @@ class ChangeMaker {
       int[] B = new int[k];
       int x;
       C[0] = 0;
-      for(x = 1; x <= n; x++)
-      {
+      for(x = 1; x <= n; x++) {
          int min = 100000;
-         for(int m = 0; m < k; m++)
-         {
-            if(x - d[m] >= 0)
-            {
-               if(C[x-d[m]] < min)
-               {
+         for(int m = 0; m < k; m++) {
+            if(x - d[m] >= 0) {
+               if(C[x-d[m]] < min) {
                   A[x] = m;
                   min = C[x-d[m]];
                }
             }
          }
+
          C[x] = 1 + min;
       }
-      System.out.print("C = ");
-      for(int y = 1; y <= n; y++)
-      {
-         System.out.print(C[y] + " ");
-      }
-      System.out.println();
+
+      // System.out.print("C = ");
+      // for(int y = 1; y <= n; y++) {
+      //    System.out.print(C[y] + " ");
+      // }
+      // System.out.println();
+
       int i = n;
-      while(i > 0)
-      {
+      while(i > 0) {
          B[A[i]]++;
          i = i - d[A[i]];
       }
       return B;
    }
 
-   public static void main(String[] args) {
-      int i;
-      System.out.println("Enter the number of coin-denominations and the set of coin values:");
-      Scanner sc = new Scanner(System.in);
-      int x = sc.nextInt();
-      int[] d = new int[x];
-      for(i = 0; i < x; i++)
-         d[i] = sc.nextInt();
-      System.out.println("Enter a positive amount to be changed (enter 0 to quit):");
-      int n = sc.nextInt();
-      if(n == 0)
-         return;
-      System.out.print("Coin array of size " + x + ": ");
-      for(i = 0; i < x; i++)
-         System.out.print("["+ d[i] + "] ");
-      System.out.println("\nAmount to be changed: " + n);
-      int[] B = new int[x];
-      try{
-         B = change_DP(n,d);
-      }
-      catch(IllegalArgumentException e) {
-         System.out.println("Input Error");
-      }
-      int cc = 0;
-      System.out.print("DP algorithm results\nAmount: " + n + "\nOptimal distribution:");
-      for(i = 0; i < x; i++)
-      {
-         if(B[i] > 0)
-         {
-            if(cc !=0)
-               System.out.print(" +");
-            System.out.print(" " + B[i] + "*" + d[i] + "c");
-            cc += B[i];
+   public static int[] change_greedy(int n, int[] d) {
+      int k = d.length, i = 0;
+      int[] B = new int[k];
+
+      while (i < k) {
+         if (d[i] <= n) {
+            B[i]++;
+            n -= d[i];
+         }
+
+         else {
+            i++;
          }
       }
-      System.out.println("\nOptimal coin count: " + cc);
-      System.out.print("B = ");
-      for(int y = 0; y < x; y++)
-      {
-         System.out.print(B[y] + " ");
+
+      return B;
+   }
+
+   public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      int i;
+      System.out.println("Enter the number of coin-denominations and the set of coin values in decreasing order:");
+      int x = sc.nextInt();
+      int[] d = new int[x];
+
+      for(i = 0; i < x; i++)
+         d[i] = sc.nextInt();
+
+      while (true) {
+         System.out.println("Enter a positive amount to be changed (enter 0 to quit):");
+         int n = sc.nextInt();
+         System.out.println();
+         
+         if(n == 0) {
+            System.out.println("Thanks for playing. Good Bye.");
+            return;
+         }
+         
+         // System.out.print("Coin array of size " + x + ": ");
+         // for(i = 0; i < x; i++)
+         //    System.out.print("["+ d[i] + "] ");
+
+         // System.out.println("\nAmount to be changed: " + n);
+         int[] B = new int[x];
+         
+         try {
+            B = change_DP(n,d);
+         }
+         catch(IllegalArgumentException e) {
+            System.out.println("Input Error");
+         }
+
+         int cc = 0;
+         System.out.print("DP algorithm results\nAmount: " + n + "\nOptimal distribution:");
+         for(i = 0; i < x; i++) {
+            if(B[i] > 0) {
+               if(cc !=0)
+                  System.out.print(" +");
+               System.out.print(" " + B[i] + "*" + d[i] + "c");
+               cc += B[i];
+            }
+         }
+
+         System.out.println("\nOptimal coin count: " + cc);
+         // System.out.print("B = ");
+         // for(int y = 0; y < x; y++) {
+         //    System.out.print(B[y] + " ");
+         // }
+
+         try {
+            B = change_greedy(n, d);
+         }
+         catch(IllegalArgumentException e) {
+            System.out.println("Input Error");
+         }
+
+         cc = 0;
+         System.out.print("\nGreedy algorithm results\nAmount: " + n + "\nOptimal distribution:");
+         for(i = 0; i < x; i++) {
+            if(B[i] > 0) {
+               if(cc !=0)
+                  System.out.print(" +");
+               System.out.print(" " + B[i] + "*" + d[i] + "c");
+               cc += B[i];
+            }
+         }
+
+         System.out.println("\nOptimal coin count: " + cc + "\n");
+         // System.out.print("B = ");
+         // for(int y = 0; y < x; y++) {
+         //    System.out.print(B[y] + " ");
+         // }
       }
-      System.out.println();
    }
 }
